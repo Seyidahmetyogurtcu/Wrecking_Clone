6e1c54d2ec4657a12e35a4214d053f82e645436f
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2023-01-30 22:07:48 +0300

    FIRST COMMENT

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wrecking_Clone.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+        void Start()
+        {
+
+        }
+
+        
+        void Update()
+        {
+
+        }
+    }
+}
